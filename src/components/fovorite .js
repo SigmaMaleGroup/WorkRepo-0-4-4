d8753e5d06ed475ecba879c3c145305ebbc66e15
jintonick <viktor.krasuyk@gmail.com>
2023-05-26 12:33:25 +0300

    site v0.3.1 favorite md

--- src/components/fovorite .js
+++ src/components/fovorite .js
@@ -26,14 +26,22 @@ function Favorite (props) {
                         <h1 className="text-[32px] font-semibold">Избранное</h1>
                     </div>
                     <div className="flex mt-[24px] w-[301px] h-[44px] justify-between items-center text-[16px] font-semibold">
-                        <button style={{color: activeButton === 1 ? "black" : "#1d1d1d80"}} onClick={() => handleButtonClick(1)}>Места</button>
-                        <button style={{color: activeButton === 2 ? "black" : "#1d1d1d80"}} onClick={() => handleButtonClick(2)}>Поездки</button>
-                        <button style={{color: activeButton === 3 ? "black" : "#1d1d1d80"}} onClick={() => handleButtonClick(3)}>Рекомендации</button>
+                        <div>
+                            <button style={{color: activeButton === 1 ? "black" : "#1d1d1d80"}} onClick={() => handleButtonClick(1)}>Места</button>
+                            <div style={{display: activeButton === 1 ? "block" : "none"}} className="h-[2px] w-[51px] bg-[#FFCF08]"></div>
+                        </div>
+                        <div>
+                            <button style={{color: activeButton === 2 ? "black" : "#1d1d1d80"}} onClick={() => handleButtonClick(2)}>Поездки</button>
+                            <div style={{display: activeButton === 2 ? "block" : "none"}} className="h-[2px] w-[68px] bg-[#FFCF08]"></div>
+                        </div>
+                        <div>
+                            <button style={{color: activeButton === 3 ? "black" : "#1d1d1d80"}} onClick={() => handleButtonClick(3)}>Рекомендации</button>
+                            <div style={{display: activeButton === 3 ? "block" : "none"}} className="h-[2px] w-[120px] bg-[#FFCF08]"></div>
+                        </div>
                     </div>
                 </div>
                 {activeButton === 1 && (
                     <div className="mt-[40px] w-[1224px] h-[974px] flex flex-wrap justify-between items-between">
-                        <div className="absolute left-[337px] top-[190px] h-[2px] w-[51px] bg-[#FFCF08]"></div>
                         <FavoriteCadrd header="Все места" paragraf="100 мест" fvbutton="Посмотреть все" fvimgmane={fvimg1}/>
                         <FavoriteCadrd header="Оренбург" paragraf="2 дня, 3 места" fvbutton="Спанировать поездку" fvimgmane={fvimg2}/>
                         <FavoriteCadrd header="Алтай" paragraf="2 дня, 3 места" fvbutton="Спанировать поездку" fvimgmane={fvimg3}/>
@@ -44,7 +52,6 @@ function Favorite (props) {
                 )}
                 {activeButton === 2 && (
                     <div className="mt-[40px] w-[1224px] h-[974px] flex flex-wrap justify-between items-between">
-                        <div className="absolute left-[423px] top-[190px] h-[2px] w-[68px] bg-[#FFCF08]"></div>
                         <FavoriteCadrd header="Все места" paragraf="100 мест" fvbutton="Отрыть план" fvimgmane={fvimg1}/>
                         <FavoriteCadrd header="аааОренбург" paragraf="2 дня, 3 места" fvbutton="Отрыть план" fvimgmane={fvimg2}/>
                         <FavoriteCadrd header="ааАлтай" paragraf="2 дня, 3 места" fvbutton="Отрыть план" fvimgmane={fvimg3}/>
@@ -55,7 +62,6 @@ function Favorite (props) {
                 )}
                 {activeButton === 3 && (
                     <div className="mt-[40px] w-[1224px] h-[974px] flex flex-wrap justify-between items-between">
-                        <div className="absolute left-[525px] top-[190px] h-[2px] w-[120px] bg-[#FFCF08]"></div>
                         <FavoriteCadrd header="ббВсе места" paragraf="100 мест" fvbutton="Посмотреть" fvimgmane={fvimg1}/>
                         <FavoriteCadrd header="ббОренбург" paragraf="2 дня, 3 места" fvbutton="Посмотреть" fvimgmane={fvimg2}/>
                         <FavoriteCadrd header="Алтай" paragraf="2 дня, 3 места" fvbutton="Посмотреть" fvimgmane={fvimg3}/>
